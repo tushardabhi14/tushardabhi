2982082d90a55e20cbc227d7e35edb084f551034
Tushar Dabhi <tushardabhi@Tushars-MacBook-Air.local>
2026-05-28 20:19:28 +0530

    Deploy Portfolio Update - 2026-05-28 08:19 PM

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1e9a811bf8e70466596bcf0ea3a8b5adb5f17f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1793772986"
+    serviceWorkerVersion: "1389606425"
   }
 });
